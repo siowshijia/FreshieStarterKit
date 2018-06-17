44ae935d5c9a92e33fe6885c32853b213fb164cc
Shijia <siowshijia@gmail.com>
2018-06-17 23:10:06 +0800

    add: page title

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,9 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->template('layouts/home');
+		$data = array(
+			'view_name' => 'Home',
+		);
+		$this->load->template('layouts/home', $data);
 	}
 }
--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Home | Triangle</title>
+    <title><?php echo isset($view_name) ? $view_name : 'Page'; ?> | NYPFreshmanStarterKit</title>
     <link href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" rel="stylesheet">
     <link href="<?php echo base_url("assets/css/font-awesome.min.css"); ?>" rel="stylesheet">
     <link href="<?php echo base_url("assets/css/animate.min.css"); ?>" rel="stylesheet">
