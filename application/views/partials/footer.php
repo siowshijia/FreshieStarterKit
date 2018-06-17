fee968b51e636d84bf74f72a1522adbacae360f1
Shijia <siowshijia@gmail.com>
2018-06-17 23:10:34 +0800

    add: rearrange footer layout

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -2,73 +2,40 @@
     <footer id="footer">
         <div class="container">
             <div class="row">
-                <div class="col-sm-12 text-center bottom-separator">
-                    <img src="<?php echo base_url() . 'assets/images/home/under.png'; ?>" class="img-responsive inline" alt="">
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="testimonial bottom">
-                        <h2>Testimonial</h2>
-                        <div class="media">
-                            <div class="pull-left">
-                                <a href="#"><img src="<?php echo base_url() . 'assets/images/home/profile1.png'; ?>" alt=""></a>
-                            </div>
-                            <div class="media-body">
-                                <blockquote>Nisi commodo bresaola, leberkas venison eiusmod bacon occaecat labore tail.</blockquote>
-                                <h3><a href="#">- Jhon Kalis</a></h3>
-                            </div>
-                         </div>
-                        <div class="media">
-                            <div class="pull-left">
-                                <a href="#"><img src="<?php echo base_url() . 'assets/images/home/profile2.png'; ?>" alt=""></a>
-                            </div>
-                            <div class="media-body">
-                                <blockquote>Capicola nisi flank sed minim sunt aliqua rump pancetta leberkas venison eiusmod.</blockquote>
-                                <h3><a href="">- Abraham Josef</a></h3>
-                            </div>
-                        </div>
+                <div class="col-md-4 col-sm-12">
+                    <div class="contact-info bottom">
+                        <h2>Site Map</h2>
+                        <ul class="p-l-none">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Profile</a></li>
+                            <li><a href="#">Quizzes</a></li>
+                            <li><a href="#">Rewards</a></li>
+                            <li><a href="#">Login</a></li>
+                            <li><a href="#">Register</a></li>
+                        </ul>
                     </div>
                 </div>
-                <div class="col-md-3 col-sm-6">
+                <div class="col-md-4 col-sm-6">
                     <div class="contact-info bottom">
                         <h2>Contacts</h2>
                         <address>
                         E-mail: <a href="mailto:someone@example.com">email@email.com</a> <br>
-                        Phone: +1 (123) 456 7890 <br>
-                        Fax: +1 (123) 456 7891 <br>
-                        </address>
-
-                        <h2>Address</h2>
-                        <address>
-                        Unit C2, St.Vincent's Trading Est., <br>
-                        Feeder Road, <br>
-                        Bristol, BS2 0UY <br>
-                        United Kingdom <br>
+                        Tel: 64515115
                         </address>
                     </div>
                 </div>
-                <div class="col-md-4 col-sm-12">
-                    <div class="contact-form bottom">
-                        <h2>Send a message</h2>
-                        <form id="main-contact-form" name="contact-form" method="post" action="sendemail.php">
-                            <div class="form-group">
-                                <input type="text" name="name" class="form-control" required="required" placeholder="Name">
-                            </div>
-                            <div class="form-group">
-                                <input type="email" name="email" class="form-control" required="required" placeholder="Email Id">
-                            </div>
-                            <div class="form-group">
-                                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your text here"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <input type="submit" name="submit" class="btn btn-submit" value="Submit">
-                            </div>
-                        </form>
+                <div class="col-md-4 col-sm-6">
+                    <div class="contact-info bottom">
+                        <h2>Address</h2>
+                        <address>
+                        180 Ang Mo Kio Avenue 8  <br>
+                        Singapore (569830)
+                        </address>
                     </div>
                 </div>
                 <div class="col-sm-12">
                     <div class="copyright-text text-center">
-                        <p>&copy; Your Company 2016. All Rights Reserved.</p>
-                        <p>Crafted by <a target="_blank" href="http://designscrazed.org/">Allie</a></p>
+                        <p>&copy; <?php echo date('Y'); ?> NYPFreshmanStarterKit. All Rights Reserved.</p>
                     </div>
                 </div>
             </div>
