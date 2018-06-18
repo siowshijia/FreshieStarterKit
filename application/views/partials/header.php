ecb0811df6d2e818ea403b8d0ee6d9f043e0e102
Lim Jing Wei <34240399+jwlim1994@users.noreply.github.com>
2018-06-18 10:37:03 +0800

    Add files via upload

---
+++ application/views/partials/header.php
@@ -0,0 +1,26 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title><?php echo isset($view_name) ? $view_name : 'Page'; ?> | NYPFreshmanStarterKit</title>
+    <link href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/font-awesome.min.css"); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/animate.min.css"); ?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/lightbox.css"); ?>" rel="stylesheet">
+	<link href="<?php echo base_url("assets/css/main.css"); ?>" rel="stylesheet">
+	<link href="<?php echo base_url("assets/css/responsive.css"); ?>" rel="stylesheet">
+	<link href="<?php echo base_url("assets/css/master.css"); ?>" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+	    <script src="js/html5shiv.js"></script>
+	    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+    <link rel="shortcut icon" href="<?php echo base_url() . 'assets/images/ico/favicon.ico'; ?>">
+</head><!--/head-->
+
+<body>
