ba034099dfa3dd40adc5520539f70c248fed1c29
Shi Jia Siow <siowshijia@gmail.com>
2018-07-22 23:44:39 +0800

    add: reward page model and controller

---
+++ application/views/layouts/reward/login.php
@@ -0,0 +1,40 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<section id="page-breadcrumb">
+    <div class="vertical-center sun">
+         <div class="container">
+            <div class="row">
+                <div class="action">
+                    <div class="col-sm-12">
+                        <h1 class="title"><?php echo $view_name; ?></h1>
+                    </div>
+                 </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--/#page-breadcrumb-->
+
+<section class="section-base">
+    <div class="container-xs">
+        <form class="form-signin" action="" method="post">
+            <h2>Please sign in:</h2>
+            <?php if (isset($error_msg)) { ?>
+                <div class="alert alert-danger text-center"><?php echo $error_msg; ?></div>
+            <?php } ?>
+            <div class="form-group">
+                <label for="stud_email" class="sr-only">Email address</label>
+                <input type="email" name="stud_email" id="stud_email" class="form-control" placeholder="Email address" required="" autofocus="">
+            </div>
+            <div class="form-group">
+                <label for="stud_pass" class="sr-only">Password</label>
+                <input type="password" name="stud_pass" id="stud_pass" class="form-control" placeholder="Password" required="">
+            </div>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+            <button class="btn btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+    </div>
+</section>
