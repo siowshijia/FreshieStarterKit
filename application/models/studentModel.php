0e7bd5a08fc8606fdd9015074e9356f2ddbae34f
Shi Jia Siow <siowshijia@gmail.com>
2018-07-22 18:36:26 +0800

    remove: comments

--- application/models/studentModel.php
+++ application/models/studentModel.php
@@ -12,15 +12,6 @@ class studentModel extends CI_Model
         parent::__construct();
     }
 
-    /**
-    * create_user function.
-    *
-    * @access public
-    * @param mixed $username
-    * @param mixed $email
-    * @param mixed $password
-    * @return bool true on success, false on failure
-    */
     public function create_user($name, $email, $password) {
 
         $data = array(
@@ -33,13 +24,6 @@ class studentModel extends CI_Model
 
     }
 
-    /**
-    * hash_password function.
-    *
-    * @access private
-    * @param mixed $password
-    * @return string|bool could be a string on success, or bool false on failure
-    */
     private function hash_password($password) {
 
         return password_hash($password, PASSWORD_BCRYPT);
