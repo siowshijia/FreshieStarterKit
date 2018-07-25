b1085de58d15d655b582f0474cd98491bc2e0d31
Shi Jia Siow <siowshijia@gmail.com>
2018-07-25 23:10:25 +0800

    add: jasny bootstrap

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -45,6 +45,7 @@
 
     <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.js"); ?>"></script>
     <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/jasny-bootstrap.min.js"); ?>"></script>
     <script type="text/javascript" src="<?php echo base_url("assets/js/lightbox.min.js"); ?>"></script>
     <script type="text/javascript" src="<?php echo base_url("assets/js/wow.min.js"); ?>"></script>
     <script type="text/javascript" src="<?php echo base_url("assets/js/main.js"); ?>"></script>
