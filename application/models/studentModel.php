99da6d2ec18c9be47ef299d965910b65b5e63d19
Lim Jing Wei <34240399+jwlim1994@users.noreply.github.com>
2018-07-27 14:18:00 +0800

    View Points Statement: Database Error
    Change Password Page: Current Password field (readonly, input type=password) shows cipher text.
    View Points Statement: Database Error 
    Error Number: 1052
    Column 'student_id' in where clause is ambiguous
    SELECT * FROM `reward_transaction` JOIN `student` ON `reward_transaction`.`student_id` = `student`.`student_id` WHERE `student_id` = 4
    Filename: C:/wamp/www/freshmanstarterkit/system/database/DB_driver.php
    Line Number: 691

--- application/models/studentModel.php
+++ application/models/studentModel.php
@@ -2,6 +2,7 @@
 /*
 * File Name: studentModel.php
 */
+
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class studentModel extends CI_Model
@@ -32,33 +33,59 @@ class studentModel extends CI_Model
 		$hash = $this->db->get()->row('password');
 
 		return $this->verify_password_hash($password, $hash);
-	}
+
+    }
 
     public function get_user_id_from_username($email) {
 
 		$this->db->select('student_id');
 		$this->db->from('student');
-		$this->db->where('student_email', $email);
+        $this->db->where('student_email', $email);
+        
 		return $this->db->get()->row('student_id');
 
 	}
 
+
+
     public function get_user($user_id) {
 
 		$this->db->from('student');
-		$this->db->where('student_id', $user_id);
+        $this->db->where('student_id', $user_id);
+        
 		return $this->db->get()->row();
 
-	}
+    }
+    
+    public function get_student_points_statement($user_id)
+ {
+ $this->db->from('reward_transaction');
+ $this->db->where('student_id', $user_id);
+ $this->db->join('student', 'reward_transaction.student_id = student.student_id');
+ $query = $this->db->get();
+ return $query->result();
+ }
+
+    public function update_userpass($id, $password) {
+
+        $data = array(
+            'password'               => $this->hash_password($password),
+        );
+
+        $this->db->where('student_id', $id);
+
+        return $this->db->update('student', $data);
+
+    }
 
     public function update_user($id, $name, $adm_number, $email, $contact_number, $interest) {
 
         $data = array(
-            'student_name'           => $name,
-            'admission_number'       => $adm_number,
-            'student_email'          => $email,
+            'student_name' => $name,
+            'admission_number' => $adm_number,
+            'student_email' => $email,
             'student_contact_number' => $contact_number,
-            'interest'               => $interest,
+            'interest' => $interest,
         );
 
         $this->db->where('student_id', $id);
@@ -78,6 +105,8 @@ class studentModel extends CI_Model
 		return password_verify($password, $hash);
 
 	}
+
 }
+
 
 ?>
