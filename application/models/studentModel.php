d6b99561715f978deda55560ea17dec8974f4cb5
Lim Jing Wei <34240399+jwlim1994@users.noreply.github.com>
2018-07-29 08:46:25 +0800

    Still showing the same Database Error

--- application/models/studentModel.php
+++ application/models/studentModel.php
@@ -61,7 +61,8 @@ class studentModel extends CI_Model
  {
  $this->db->from('reward_transaction');
  $this->db->where('student_id', $user_id);
- $this->db->join('student', 'reward_transaction.student_id = student.student_id');
+ $this->db->join('student', 'reward_transaction.student_id = student.student_id', 'inner');
+ $this->db->join('rewards', 'reward_transaction.reward_id = student.reward_id', 'inner');
  $query = $this->db->get();
  return $query->result();
  }
