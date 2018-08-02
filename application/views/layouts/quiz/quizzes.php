db0b560c741f58eba7928d6f0ebbe18705ea3296
Shi Jia Siow <siowshijia@gmail.com>
2018-08-03 02:10:30 +0800

    add: quizzes page

--- application/views/layouts/quiz/quizzes.php
+++ application/views/layouts/quiz/quizzes.php
@@ -18,10 +18,41 @@
 <section class="section-base">
     <div class="container-md">
         <div class="row">
-            <div class="col-xs-12 col-sm-6 col-md-3">
+            <div class="col-xs-12 col-sm-6 col-md-4 col-md-offset-2 m-b-md">
                 <div class="quiz-box">
-                    <h1>Quiz 1</h1>
-                    <h3>Title</h3>
+                    <div class="front">
+                        <div class="box1">
+                            <div class="icon-circle"><span class="fa fa-list-ol"></span></div>
+                            <h2>Quiz 1</h2>
+                            <p>Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum.</p>
+                            <a href="#" class="btn btn-common visible-xs visible-sm m-t-md">Read More</a>
+                        </div>
+                    </div>
+
+                    <div class="back">
+                        <div class="box2">
+                            <a href="#" class="btn">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-4 m-b-md">
+                <div class="quiz-box">
+                    <div class="front">
+                        <div class="box1">
+                            <div class="icon-circle"><span class="fa fa-list-ol"></span></div>
+                            <h2>Quiz 2</h2>
+                            <p>Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum.</p>
+                            <a href="#" class="btn btn-common visible-xs visible-sm m-t-md">Read More</a>
+                        </div>
+                    </div>
+
+                    <div class="back">
+                        <div class="box2">
+                            <a href="#" class="btn">Read More</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
