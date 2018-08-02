b288c6528dd1e5ddd52833c228908bd8e338c4f3
Shi Jia Siow <siowshijia@gmail.com>
2018-08-02 16:40:15 +0800

    add: validation text

--- application/views/layouts/admin/reward/add.php
+++ application/views/layouts/admin/reward/add.php
@@ -21,14 +21,17 @@
                 <div class="form-group">
                     <label for="name" class="sr-only">Name</label>
                     <input type="text" name="name" id="name" class="form-control" placeholder="Name">
+                    <span class="text-danger"><?php echo form_error('name'); ?></span>
                 </div>
                 <div class="form-group">
                     <label for="points" class="sr-only">Cost Points</label>
                     <input type="text" name="points" id="points" class="form-control" placeholder="Cost Points">
+                    <span class="text-danger"><?php echo form_error('points'); ?></span>
                 </div>
                 <div class="form-group">
                     <label for="qty" class="sr-only">Quantity</label>
                     <input type="text" name="qty" id="qty" class="form-control" placeholder="Quantity">
+                    <span class="text-danger"><?php echo form_error('qty'); ?></span>
                 </div>
                 <div class="form-group text-center">
                     <div class="fileinput fileinput-new" data-provides="fileinput">
@@ -42,15 +45,18 @@
                             <a href="#" class="btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
                         </div>
                         <p class="small m-b-none"><i>*Max. file size: 1MB</i></p>
+                        <span class="text-danger"><?php echo form_error('image'); ?></span>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="description" class="sr-only">Description</label>
                     <textarea name="description" id="description" class="form-control" rows="8" cols="80" placeholder="Description"></textarea>
+                    <span class="text-danger"><?php echo form_error('description'); ?></span>
                 </div>
                 <div class="form-group">
                     <label for="expired_date" class="sr-only">Expired Date</label>
                     <input type="text" name="expired_date" id="expired_date" class="form-control datepicker" placeholder="Expired Date">
+                    <span class="text-danger"><?php echo form_error('expired_date'); ?></span>
                 </div>
                 <button class="btn btn-primary btn-block" type="submit">Add</button>
                 <a href="<?php echo base_url('/admin/reward/dashboard'); ?>" class="btn btn-primary btn-block m-t-sm">Back</a>
--- application/views/layouts/admin/student/add.php
+++ application/views/layouts/admin/student/add.php
@@ -24,30 +24,37 @@
                 <div class="form-group">
                     <label for="name" class="sr-only">Name</label>
                     <input type="text" name="name" id="name" class="form-control" placeholder="Name">
+                    <span class="text-danger"><?php echo form_error('name'); ?></span>
                 </div>
                 <div class="form-group">
                     <label for="admission_number" class="sr-only">Admission Number</label>
                     <input type="text" name="admission_number" id="admission_number" class="form-control" placeholder="Admission Number">
+                    <span class="text-danger"><?php echo form_error('admission_number'); ?></span>
                 </div>
                 <div class="form-group">
                     <label for="email" class="sr-only">Email address</label>
                     <input type="email" name="email" id="email" class="form-control" placeholder="Email address">
+                    <span class="text-danger"><?php echo form_error('email'); ?></span>
                 </div>
                 <div class="form-group">
                     <label for="contact_number" class="sr-only">Contact Number</label>
                     <input type="text" name="contact_number" id="contact_number" class="form-control" placeholder="Contact Number">
+                    <span class="text-danger"><?php echo form_error('contact_number'); ?></span>
                 </div>
                 <div class="form-group">
                     <label for="interest" class="sr-only">Interest</label>
                     <textarea name="interest" id="interest" class="form-control" rows="8" cols="80" placeholder="Interest"></textarea>
+                    <span class="text-danger"><?php echo form_error('interest'); ?></span>
                 </div>
                 <div class="form-group">
                     <label for="points" class="sr-only">Points</label>
                     <input type="text" name="points" id="points" class="form-control" placeholder="Points">
+                    <span class="text-danger"><?php echo form_error('points'); ?></span>
                 </div>
                 <div class="form-group">
                     <label for="password" class="sr-only">Password</label>
                     <input type="password" name="password" id="password" class="form-control" placeholder="Password">
+                    <span class="text-danger"><?php echo form_error('password'); ?></span>
                 </div>
                 <button class="btn btn-primary btn-block" type="submit">Add</button>
                 <a href="<?php echo base_url('/admin/student/dashboard'); ?>" class="btn btn-primary btn-block m-t-sm">Back</a>
